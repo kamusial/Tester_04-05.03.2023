c52bbb91c0934065cdd9149e44b58a3048e7778a
musiakam <kamil.musial@tietoevry.com>
2023-03-05 09:13:36 +0100

    10

--- selenium1.py
+++ selenium1.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
 import time
+import datetime
 
-driver = webdriver.Chrome()
-driver.get('https://google.com')
-print('Nazwa strony',driver.title)
-time.sleep(2)
-button1_accept = driver.find_element('id','L2AGLb')
-button1_accept.click()
-print(button1_accept)
-search_field = driver.find_element('name','q')
-search_field.send_keys('Czy jutro jest niedziela handowa?')
-#search_field.send_keys(Keys.ENTER)
-search_button = driver.find_element('name', 'btnK')
-search_button.submit()
-time.sleep(2)
-driver.quit()
+def make_screenshot(okno_przegladarki):
+    teraz = datetime.datetime.now()
+    screenshot = 'screenshot'+teraz.strftime('%H%M%S')+'.png'
+    okno_przegladarki.get_screenshot_as_file(screenshot)
+
+if __name__=='__main__':
+    driver = webdriver.Chrome()
+    driver.get('https://google.com')
+    print('Nazwa strony',driver.title)
+    time.sleep(2)
+    button1_accept = driver.find_element('id','L2AGLb')
+    button1_accept.click()
+    print(button1_accept)
+    search_field = driver.find_element('name','q')
+    search_field.send_keys('Czy jutro jest niedziela handowa?')
+    #search_field.send_keys(Keys.ENTER)
+    search_button = driver.find_element('name', 'btnK')
+    search_button.submit()
+    time.sleep(2)
+    driver.quit()
--- selenium_OOP2.py
+++ selenium_OOP2.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 from selenium_OOP import LoginPage
 import time
+from selenium1 import make_screenshot
 
 driver = webdriver.Chrome()
 page = LoginPage(driver)
 page.open()
+time.sleep(0.5)
 page.enter_username('standard_user')
+time.sleep(0.5)
 page.enter_password('secret_sauce')
+time.sleep(0.5)
 page.click_login()
-time.sleep(2)
+time.sleep(0.5)
+make_screenshot(driver)
+driver.quit()
